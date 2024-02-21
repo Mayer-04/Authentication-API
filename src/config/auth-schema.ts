2682fcc03c4e5a1e1e359bc97933ce949e88ab10
Mayer Andres <andres.20.chaves@gmail.com>
2024-02-21 00:24:47 -0500

    Nuevas validaciones

--- src/config/auth-schema.ts
+++ src/config/auth-schema.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
 
-export const registerSchema = z.object({
-  username: z.string().min(4),
-  email: z.string().email(),
-  password: z.string().min(6),
-  passwordConfirmation: z.string().min(6),
-});
+export const registerSchema = z
+  .object({
+    username: z.string().min(4),
+    email: z.string().email(),
+    password: z.string().min(6),
+    passwordConfirmation: z.string().min(6),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ["passwordConfirmation"],
+  });
 
 export const loginSchema = z.object({
   email: z.string().email(),
