39aefe8fbd3bb393f25807de730d3fe8ef94e029
Mayer-04 <andres.20.chaves@gmail.com>
2024-04-25 20:07:14 -0500

    Agregando nuevo puerto a CORS

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,8 @@
 export const PROCESS_ENV = process.env;
 export const ACCEPTED_ORIGINS = [
+  "http://localhost:5173",
   "http://localhost:5000",
-  "http://localhost:5001",
+  "http://localhost:4321",
   "http://localhost:3000",
 ];
 export const TOKEN_EXPIRATION = "1h";
